4a24a3db1f5154c702dbaa8ba20d0ae7134af67f
Gabe <gafopi@gmail.com>
2016-11-06 01:07:25 -0400

    Improved example cli app

--- src/main/java/cloud/toby/App.java
+++ src/main/java/cloud/toby/App.java
@@ -15,6 +15,10 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -29,7 +33,7 @@ public class App {
     private static class OnConnect implements OnConnectCallback {
       public void go(Bot bot) {
         try {
-          System.out.println("Connected!");
+          System.out.println("Connected! " + new Date().toString());
           bot.info("info"); // get info so we know what type of bot
         } catch (NotConnectedException e) {
           System.out.println("not connected");
@@ -52,10 +56,8 @@ public class App {
      */
     private static class OnMessage implements OnMessageCallback {
       public void go(Bot bot, String from, Message message) {
-        System.out.print("Message received: ");
-        System.out.println(from);
-        System.out.println(message);
-
+        System.out.println(String.format("\b\b\b\bMessage received: %s %s", from, message));
+        System.out.print(">>> ");
       }
 
       // TODO
@@ -74,7 +76,6 @@ public class App {
       }
     }
 
-
     /**
      * handleInfoResponse - called when we receive information response from server
      *
@@ -85,7 +86,6 @@ public class App {
       // start main menu for the given bot type
       mainMenu(bot, payload.get("type").toString());
     }
-
 
     /**
      * getBotCommands - return the commands for a bot of given type
@@ -101,8 +101,6 @@ public class App {
       }
       return null;
     }
-
-
 
     /**
      * mainMenu - the main menu loop
@@ -123,46 +121,54 @@ public class App {
         List<String> commandSplit = new ArrayList<String>();
         Collections.addAll(commandSplit, command.split(" "));
 
-        // NOTE: there are no checks. if a bot tries to invoke a command that
-        // they don't have permission for, they will be kicked off by the server
-        switch (commandSplit.get(0)) {
-          case "s":
-          case "send":
-            System.out.println("bot wants to send."); break;
-          case "c":
-          case "create":
-            System.out.println("bot wants to create."); break;
-          case "r":
-          case "remove":
-            System.out.println("bot wants to remove."); break;
-          case "f":
-          case "follow":
-            System.out.println("bot wants to follow."); break;
-          case "u":
-          case "unfollow":
-            System.out.println("bot wants to unfollow."); break;
-          case "n":
-          case "no":
-            System.out.println("bot wants to turn hooks off"); break;
-          case "h":
-          case "hooks":
-            System.out.println("bot wants to turn hooks on"); break;
-          case "cl":
-          case "clear":
-            clear(); break;
-          case "q":
-          case "quit":
-            bot.end();
-            System.exit(1);
-          case "":
-            break;
-          default:
-            System.out.println("-toby: " + commandSplit.get(0) + ": command not found");
-        }
+        try {
 
+            // NOTE: there are no checks. if a bot tries to invoke a command that
+            // they don't have permission for, they will be kicked off by the server
+            switch (commandSplit.get(0)) {
+                case "s":
+                case "send":
+                  String message = command.substring(commandSplit.get(0).length());
+                  String messageNoTags = removeTags(message, "#");
+                  if (messageNoTags.length() > 0)
+                    bot.send(new Message(messageNoTags, "TEXT", extractTags(message, "#")));
+                  break;
+                case "c":
+                case "create":
+                  System.out.println("bot wants to create."); break;
+                case "r":
+                case "remove":
+                  System.out.println("bot wants to remove."); break;
+                case "f":
+                case "follow":
+                  System.out.println("bot wants to follow."); break;
+                case "u":
+                case "unfollow":
+                  System.out.println("bot wants to unfollow."); break;
+                case "n":
+                case "no":
+                  System.out.println("bot wants to turn hooks off"); break;
+                case "h":
+                case "hooks":
+                  System.out.println("bot wants to turn hooks on"); break;
+                case "cl":
+                case "clear":
+                  clear(); break;
+                case "q":
+                case "quit":
+                  bot.end();
+                  System.exit(1);
+                case "":
+                  break;
+                default:
+                  System.out.println("-toby: " + commandSplit.get(0) + ": command not found");
+            }
+
+        } catch (Exception e) {
+          System.out.println("error");
+        }
       }
     }
-
 
     /**
      * prompt - prompt console input
@@ -175,20 +181,53 @@ public class App {
       return System.console().readLine();
     }
 
+    /**
+     * clear - clear console and print Toby ASCII
+     */
     private static void clear() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
+        System.out.println(" ▄▀▀▀█▀▀▄  ▄▀▀▀▀▄   ▄▀▀█▄▄   ▄▀▀▄ ▀▀▄\n█    █  ▐ █      █ ▐ ▄▀   █ █   ▀▄ ▄▀\n▐   █     █      █   █▄▄▄▀  ▐     █\n   █      ▀▄    ▄▀   █   █        █\n ▄▀         ▀▀▀▀    ▄▀▄▄▄▀      ▄▀\n█                  █    ▐       █\n▐                  ▐            ▐\n");
     }
 
-    public static void main( String[] args ) {
+    /**
+     * extractTags - extract the tags from a string
+     *
+     * @param  String text      the text to extract tags from
+     * @param  String delimiter the tag delimiter
+     * @return {List<String}    a list of extracted tags
+     */
+    private static List<String> extractTags(String text, String delimiter) {
+       Pattern p = Pattern.compile(String.format("(?:^|\\s|[\\p{Punct}&&[^/]])(%s[\\p{L}0-9-_]+)", delimiter));
+       Matcher m = p.matcher(text);   // get a matcher object
+       List<String> tags = new ArrayList<String>();
+       while (m.find()) {
+         tags.add(m.group().trim().substring(1));
+       }
+       return tags;
+    }
 
+    /**
+     * private - description
+     *
+     * @param  {String} text      the text to remove tags from
+     * @param  {String} delimiter the tag delimiter
+     * @return {String}           the text without tags
+     */
+    private static String removeTags(String text, String delimiter) {
+      return text.replaceAll(String.format("(?:^|\\s|[\\p{Punct}&&[^/]])(%s[\\p{L}0-9-_]+)", delimiter), "").trim();
+    }
+
+    /**
+     * Main
+     */
+    public static void main( String[] args ) {
 
       // prompt for credentials
       String username = prompt("username: ");
       String password = prompt("password: ");
 
-      clear();
-      System.out.println(" ▄▀▀▀█▀▀▄  ▄▀▀▀▀▄   ▄▀▀█▄▄   ▄▀▀▄ ▀▀▄\n█    █  ▐ █      █ ▐ ▄▀   █ █   ▀▄ ▄▀\n▐   █     █      █   █▄▄▄▀  ▐     █\n   █      ▀▄    ▄▀   █   █        █\n ▄▀         ▀▀▀▀    ▄▀▄▄▄▀      ▄▀\n█                  █    ▐       █\n▐                  ▐            ▐\n");
+      clear(); // clear console
 
       Bot bot = new Bot(username, password, new OnConnect(), new OnDisconnect(), new OnMessage());
       bot.start();
